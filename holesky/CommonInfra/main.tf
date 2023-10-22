b5a2fcdbb7979fe164d6a95b4db35fc2bec92c40
ManInWeb3 <16667906+ManInWeb3@users.noreply.github.com>
2023-10-23 11:17:11 +1300

    dont create separate acl

--- holesky/CommonInfra/main.tf
+++ holesky/CommonInfra/main.tf
@@ -18,7 +18,7 @@ module "vpc" {
   public_subnets      = [for k, v in var.azs : cidrsubnet(local.vpc_cidr, 8, k + 4)]
   # elasticache_subnets = [for k, v in var.azs : cidrsubnet(local.vpc_cidr, 8, k + 8)]
 
-  public_dedicated_network_acl   = true
+  # public_dedicated_network_acl   = true
   public_inbound_acl_rules       = local.network_acls["public_inbound"]  #concat(local.network_acls["default_inbound"], local.network_acls["public_inbound"])
   public_outbound_acl_rules      = local.network_acls["public_outbound"] #concat(local.network_acls["default_outbound"], local.network_acls["public_outbound"])
 
