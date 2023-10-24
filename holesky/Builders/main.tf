a1a0f76ef40dd142f6e6170470661176e80e1214
ManInWeb3 <16667906+ManInWeb3@users.noreply.github.com>
2023-10-24 16:59:11 +1300

    Change image to have ssm agent installed

--- holesky/Builders/main.tf
+++ holesky/Builders/main.tf
@@ -9,7 +9,7 @@ data "aws_ami" "ubuntu2204" {
 
   filter {
     name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-*"]
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
   }
 }
 
