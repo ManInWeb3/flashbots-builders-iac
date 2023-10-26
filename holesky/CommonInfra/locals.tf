07cb6f0cb410cc106111a6540f6c0e7bcad4b86b
ManInWeb3 <16667906+ManInWeb3@users.noreply.github.com>
2023-10-26 14:13:10 +1300

    azs are list

--- holesky/CommonInfra/locals.tf
+++ holesky/CommonInfra/locals.tf
@@ -3,7 +3,7 @@ locals {
   ###* Sometimes it's more convinient to define input values as locals, instead of variables
   ###* this could be more convinient because: you can see all the values in a shorter form in one file - kinda a config file
   region = "us-east-1"
-  azs    = "us-east-1a"
+  azs    = ["us-east-1a"]
 
   ethereum_network = "holesky"
   vpc_cidr = "10.0.0.0/16"
