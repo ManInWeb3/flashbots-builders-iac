077d78d4f2449a55a1194faaa5c142a1e9c05dc3
ManInWeb3 <16667906+ManInWeb3@users.noreply.github.com>
2023-10-27 12:05:47 +1300

    Formatting and commenting

--- holesky/Builders/main.tf
+++ holesky/Builders/main.tf
@@ -5,7 +5,8 @@ locals{
 module "builders" {
   source  = "../../modules/Builders"
 
-  #* BUILDER release to deploy. For example: v1.13.2-4844.dev5.c786eb74f
+  #* To deploy a new builder release, change this value.
+  #* For example: v1.13.2-4844.dev5.c786eb74f
   builder_release = "v1.13.2-4844.dev5.c786eb74f"
 
   #* Builder command already have the following arguments auto configured:
@@ -13,7 +14,7 @@ module "builders" {
   #* --authrpc.jwtsecret=$BUILDER_JWT_PATH
   #* --datadir=$DATA_DIR/${ethereum_network}/$(basename $BUILDER_BIN)
   #* --log.file=$DATA_DIR/${ethereum_network}/$(basename $BUILDER_BIN)_$BUILDER_RELEASE.log
-  #* If you need to add arguments, add them in builder_AdditionalArgs list
+  #* If you need to add more arguments, add them in builder_AdditionalArgs list
   builder_AdditionalArgs = [
     "--http --http.api eth,net,engine,admin",
     "--cache=256", # Only on small RAM vm
@@ -24,27 +25,30 @@ module "builders" {
   ]
 
   # prysm_release   = null #"v4.1.0"
+  #* Beacon node release tag
   nimbus_release  = "v23.10.0"
 
+  #* Ethereum network to deploy
   ethereum_network = "holesky"
-  region           = local.region   # See above
-  vpc_id           = "vpc-085701af4f385bba2"
 
+  #* List of builders to deploy
   builder_instances = {
+    #* Example builder
+    #* "builder_name" = {
+    #*   subnet_id = "Subnet id to deploy the instance"
+    #* },
+    #* builder_name - Name of the instance and secret name, which stores the secret key ($BUILDER_TX_SIGNING_KEY) to run builder with
     "builder-111_333" = {
       subnet_id = "subnet-05a71022cf5148113"
     },
-    # key_222_333 = {
-    #   subnet_id = "dsfsdfsdfsd"
-    # },
   }
 
-  # Commont Builders settings
-  ssh_key_name = "vlad"
-  builders_instance_type = "t3.medium"  #"t3.micro"
+  #* Builders common settings
+  ssh_key_name           = "vlad"                  # Only to rsync data, to access instance's console use Session manager
+  builders_instance_type = "t3.medium"             #"t3.micro"
   root_volume_size       = 20
   data_volume_size       = 10
-
-  # Security groups
-  vpc_endpoints_security_group_id = "sg-00fbe09a6771db7e2"
+  region                 = local.region            # See value in locals above
+  vpc_id                 = "vpc-085701af4f385bba2"
+  vpc_endpoints_security_group_id = "sg-00fbe09a6771db7e2"   # VPC Endpoint security group, created in common infra module
 }
