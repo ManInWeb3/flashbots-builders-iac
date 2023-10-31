742b10312d9531b84c8a15fa334da974fca4fc4d
ManInWeb3 <16667906+ManInWeb3@users.noreply.github.com>
2023-10-31 14:28:17 +1300

    move values to main module

--- holesky/CommonInfra/main.tf
+++ holesky/CommonInfra/main.tf
@@ -1,9 +1,15 @@
+
+locals {
+  #* Define in locals to re-use in provider
+  region = "us-east-1"
+}
+
 module "vpc_holesky" {
   source  = "../../modules/CommonInfra"
 
-  azs      = local.azs
+  azs      = ["us-east-1a"]
   region   = local.region
 
-  vpc_cidr = local.vpc_cidr
-  ethereum_network = local.ethereum_network
+  vpc_cidr = "10.0.0.0/16"
+  ethereum_network = "holesky"
 }
